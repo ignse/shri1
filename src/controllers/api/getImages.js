aaafbcf562de73ca00417f242ebbbdedbd433481
ignse <85495590+ignse@users.noreply.github.com>
2021-10-03 03:41:55 +0300

    list issue fix

--- src/controllers/api/getImages.js
+++ src/controllers/api/getImages.js
@@ -1,7 +1,8 @@
 const db = require('../../entities/Database');
+const Image = require('../../entities/Image');
 
 module.exports = (req, res) => {
-  const allImages = db.find().map((image) => image.toPublicJSON());
+  const allImages = db.find().map((image) => new Image(image).toPublicJSON());
 
   return res.json(allImages);
 };
